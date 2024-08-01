1e84fcf13f80c133ae6bf03c1c4d63d3ed883886
OnlyExpLine <onlyling1234@gmail.com>
2024-08-01 18:10:24 +0300

    TextFieldCommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gym_app_project/app/FitAsPage.dart';
+import 'package:gym_app_project/components/TextFieldComponents.dart';
 import 'constant.dart';
 void main() {
   runApp( MaterialApp(
     theme: ThemeData(
         primaryColor: indigoColor,
     scaffoldBackgroundColor: deepPurple50),
-    home: FitAsPage(),
+    home: TextFieldComponents(),
   ));
 }
